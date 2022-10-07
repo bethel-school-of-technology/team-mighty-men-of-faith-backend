bdf2d7d020c1e5c2435ccac6ac97dfd68fb149a7
CJohnson5781 <jjakacjj@yahoo.com>
2022-10-07 13:40:20 -0500

    successful db connection for user

---
+++ build/models/vehicle.js
@@ -0,0 +1,56 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.VehicleFactory = exports.Vehicle = void 0;
+const sequelize_1 = require("sequelize");
+class Vehicle extends sequelize_1.Model {
+}
+exports.Vehicle = Vehicle;
+function VehicleFactory(sequelize) {
+    Vehicle.init({
+        vehicleID: {
+            type: sequelize_1.DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+            allowNull: false
+        },
+        ownerID: {
+            type: sequelize_1.DataTypes.INTEGER,
+            allowNull: false,
+        },
+        make: {
+            type: sequelize_1.DataTypes.STRING,
+            allowNull: false,
+        },
+        model: {
+            type: sequelize_1.DataTypes.STRING,
+            allowNull: false,
+        },
+        year: {
+            type: sequelize_1.DataTypes.INTEGER,
+            allowNull: false,
+        },
+        photoURL: {
+            type: sequelize_1.DataTypes.STRING,
+            allowNull: false,
+        },
+        location: {
+            type: sequelize_1.DataTypes.STRING,
+            allowNull: false,
+        },
+        createdAt: {
+            type: sequelize_1.DataTypes.DATE,
+            allowNull: false,
+            defaultValue: sequelize_1.DataTypes.NOW,
+        },
+        updatedAt: {
+            type: sequelize_1.DataTypes.DATE,
+            allowNull: false,
+            defaultValue: sequelize_1.DataTypes.NOW,
+        }
+    }, {
+        tableName: 'vehicles',
+        freezeTableName: true,
+        sequelize
+    });
+}
+exports.VehicleFactory = VehicleFactory;
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,19 +1,19 @@
-// import { Sequelize } from "sequelize";
-// // import { AssociateUserReservation, ReservationFactory } from "./reservation";
-// import { UserFactory } from "./user";
+import { Sequelize } from "sequelize";
+// import { AssociateUserReservation, ReservationFactory } from "./reservation";
+import { UserFactory } from "./user";
 
-// const dbName = 'testDB';
-// const username = 'ourdb';
-// const password = 'Fortynine9';
+const dbName = 'testDB';
+const username = 'ourdb';
+const password = 'Fortynine9';
 
-// const sequelize = new Sequelize(dbName, username, password, {
-//     host: 'localhost',
-//     port: 3306,
-//     dialect: 'mysql'
-// });
+const sequelize = new Sequelize(dbName, username, password, {
+    host: 'localhost',
+    port: 3306,
+    dialect: 'mysql'
+});
 
-// UserFactory(sequelize);
-// // ReservationFactory(sequelize);
-// // AssociateUserReservation();
+UserFactory(sequelize);
+// ReservationFactory(sequelize);
+// AssociateUserReservation();
 
-// export const db = sequelize;
+export const db = sequelize;

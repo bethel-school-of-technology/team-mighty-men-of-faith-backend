7ccafd34fa3ae9f6a74170b3bdd70a87c8f75806
CJohnson5781 <jjakacjj@yahoo.com>
2022-10-06 19:58:29 -0500

    made index.ts model for connecting sql

---
+++ src/models/index.ts
@@ -0,0 +1,19 @@
+// import { Sequelize } from "sequelize";
+// // import { AssociateUserReservation, ReservationFactory } from "./reservation";
+// import { UserFactory } from "./user";
+
+// const dbName = 'testDB';
+// const username = 'ourdb';
+// const password = 'Fortynine9';
+
+// const sequelize = new Sequelize(dbName, username, password, {
+//     host: 'localhost',
+//     port: 3306,
+//     dialect: 'mysql'
+// });
+
+// UserFactory(sequelize);
+// // ReservationFactory(sequelize);
+// // AssociateUserReservation();
+
+// export const db = sequelize;

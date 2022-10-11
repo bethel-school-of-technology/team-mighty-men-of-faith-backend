7968400ac878da862c0942709a196628a4e5aaae
jimandjo <jimandjo.geo@yahoo.com>
2022-10-11 17:26:55 -0400

    Corrected the addVehicle Controller

--- src/controllers/vehicleController.ts
+++ src/controllers/vehicleController.ts
@@ -3,9 +3,17 @@ import { Vehicle } from "../models/vehicle";
 
 export const addVehicle: RequestHandler = async (req, res, next) => {
     let newVehicle: Vehicle = req.body;
-    if (newVehicle.vehicleID && newVehicle.vehicleID + newVehicle.make + newVehicle.model + newVehicle.year + newVehicle.photoURL + newVehicle.location + newVehicle.createdAt + newVehicle.updatedAt) {
+    if (newVehicle.vehicleID && newVehicle.vehicleID) {
         let created = await Vehicle.create(newVehicle);
-        res.status(201).json(created);
+        res.status(201).json({
+            make: created.make,
+            model: created.model,
+            year: created.year,
+            photoURL: created.photoURL,
+            location: created.location,
+            createdAT: created.createdAt,
+            updatedAT: created.updatedAt
+        });
     }
     else {
         res.status(400).send();

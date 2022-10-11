ccb6c83365efa286b1b18e69373440b4e2b4c564
jimandjo <jimandjo.geo@yahoo.com>
2022-10-11 17:49:59 -0400

    added data to the json part of vehicle controller

--- build/controllers/vehicleController.js
+++ build/controllers/vehicleController.js
@@ -4,9 +4,17 @@ exports.addVehicle = void 0;
 const vehicle_1 = require("../models/vehicle");
 const addVehicle = async (req, res, next) => {
     let newVehicle = req.body;
-    if (newVehicle.vehicleID && newVehicle.vehicleID + newVehicle.make + newVehicle.model + newVehicle.year + newVehicle.photoURL + newVehicle.location + newVehicle.createdAt + newVehicle.updatedAt) {
+    if (newVehicle.vehicleID && newVehicle.vehicleID) {
         let created = await vehicle_1.Vehicle.create(newVehicle);
-        res.status(201).json(created);
+        res.status(201).json({
+            make: created.make,
+            model: created.model,
+            year: created.year,
+            photoURL: created.photoURL,
+            location: created.location,
+            createdAT: created.createdAt,
+            updatedAT: created.updatedAt
+        });
     }
     else {
         res.status(400).send();
